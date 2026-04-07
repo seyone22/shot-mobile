f348f3b4a437d488755d4c94b77e5408b3e3a8b1
Seyone Gunasingham <s.g.seyone@proton.me>
2026-04-07 10:48:17 +0530

    Changed imports

--- mobile/src/main/java/dev/seyone/shot/ui/screen/more/ArchersListScreen.kt
+++ mobile/src/main/java/dev/seyone/shot/ui/screen/more/ArchersListScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.outlined.ArrowBack
 import androidx.compose.material.icons.filled.Add
