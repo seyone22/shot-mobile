bbd866a1edaad5a9394b6b7f35b9de15d9a57c72
Seyone Gunasingham <s.g.seyone@proton.me>
2026-08-17 05:55:08 +0530

    chore: bump Wear OS versionCode to 20005

--- wear/build.gradle.kts
+++ wear/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "dev.seyone.shot"
         minSdk = 30
         targetSdk = 36
-        versionCode = 2
+        versionCode = 20005
         versionName = "1.1.0"
     }
 
